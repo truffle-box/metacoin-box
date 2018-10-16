1639ff026f104fbf4325da2656d37dc373e2b032
wbt <wbt@users.noreply.github.com>
2018-10-16 17:49:18 -0400

    Add default networks in config file

--- truffle.js
+++ truffle.js
@@ -1,4 +1,17 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  // The defaults below provide for an easier quick-start with Ganache. 
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+    test: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    }
+  }
 };
