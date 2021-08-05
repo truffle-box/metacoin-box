929a4115ec204ef0e9489f0e0ea24257eeef2dd2
cds-amal <cds.sudama@gmail.com>
2021-08-05 18:13:17 -0400

    use same db settings as truffle init

--- truffle-config.js
+++ truffle-config.js
@@ -17,23 +17,25 @@ module.exports = {
   //    network_id: "*"
   //  }
   //},
-
-  //db: {
-  //  // Truffle-DB is disabled by default. 
-  //  enabled: true,
-  //  host: "127.0.0.1",
-
-  //  // You can also use a local db storage adapter by uncommenting this section
-  //  //
-  //  adapter: {
-  //    name: "sqlite",
-  //    settings: {
-  //      // You may specify a directory name that is an absolute path, or one
-  //      // relative to the Project Root, the same folder where this file is
-  //      // located. For example, the following defines a `.db` directory in
-  //      // Project Root.
-  //      directory: ".db"  
-  //    }
-  //  }                             
-  //}
+  //
+  // Truffle DB is currently disabled by default; to enable it, change enabled:
+  // false to enabled: true. The default storage location can also be
+  // overridden by specifying the adapter settings, as shown in the commented code below.
+  //
+  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
+  // make a backup of your artifacts to a safe location before enabling this feature.
+  //
+  // After you backed up your artifacts you can utilize db by running migrate as follows: 
+  // $ truffle migrate --reset --compile-all
+  //
+  // db: {
+    // enabled: false,
+    // host: "127.0.0.1",
+    // adapter: {
+    //   name: "sqlite",
+    //   settings: {
+    //     directory: ".db"
+    //   }
+    // }
+  // }
 };
