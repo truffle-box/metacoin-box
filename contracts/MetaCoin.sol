7786f4baab86f60268ff3710f53723b89f4c0a51
emilyjlin95 <17280653+emilyJLin95@users.noreply.github.com>
2022-07-07 14:10:21 -0700

    Clean up MetaCoin box

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.25 <0.7.0;
+// Tells the Solidity compiler to compile only from v0.8.13 to v0.9.0
+pragma solidity ^0.8.13;
 
 import "./ConvertLib.sol";
 
 // This is just a simple example of a coin-like contract.
-// It is not standards compatible and cannot be expected to talk to other
-// coin/token contracts. If you want to create a standards-compliant
-// token, see: https://github.com/ConsenSys/Tokens. Cheers!
+// It is not ERC20 compatible and cannot be expected to talk to other
+// coin/token contracts.
 
 contract MetaCoin {
 	mapping (address => uint) balances;
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-	constructor() public {
+	constructor() {
 		balances[tx.origin] = 10000;
 	}
 
