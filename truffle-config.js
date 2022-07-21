3b1b1acc49533d4e6b20560bf883856a79b93ff3
liang liang <lsqd3v@gmail.com>
2022-07-21 15:08:11 -0400

    command out HDWalletProvider & network

--- truffle-config.js
+++ truffle-config.js
@@ -21,8 +21,8 @@
 // require('dotenv').config();
 // const mnemonic = process.env["MNEMONIC"];
 // const infuraProjectId = process.env["INFURA_PROJECT_ID"];
- 
-const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   /**
@@ -47,11 +47,11 @@ module.exports = {
 //      port: 8545,            // Standard Ethereum port (default: none)
 //      network_id: "*",       // Any network (default: none)
 //     },
-    goerli: {
-      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraProjectId}`),
-      network_id: 5,       // Goerli's id
-      chain_id: 5
-    }
+    // goerli: {
+    //   provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraProjectId}`),
+    //   network_id: 5,       // Goerli's id
+    //   chain_id: 5
+    // }
   },
 
   // Set default mocha options here, use special reporters etc.
